ec4c000069484aabf64dad2327e492de77584ca3
Matthew C <cnymhw86@gmail.com>
2023-01-18 17:35:16 -0500

    added post method and removed some variables

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const api = require('./routes/apiRoutes.js');
-const api = require('./routes/htmlRoutes.js');
 
+
+let notes = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -20,6 +21,21 @@ app.get('/', (req, res) =>
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) =>
+    res.json(notes)
+);
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = Math.floor(Math.random() * 1000000)
+    notes.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes)
+});
+
+// app.get('/api/notes/:id', (req, res) => {
+
+// });
 
 app.listen(PORT, () =>
         console.log(`View at http://localhost:${PORT}`)
